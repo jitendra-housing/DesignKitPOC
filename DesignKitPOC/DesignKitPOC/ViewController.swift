d9a6872a32d5c7ec93985e70ac21c7ad44ed7cd9
jitendra-housing <jitendra.lakhmani1@housing.com>
2025-08-25 11:40:10 +0530

    Initial project creation

---
+++ DesignKitPOC/DesignKitPOC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DesignKitPOC
+//
+//  Created by  Jitendra Lakhmani on 25/08/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
